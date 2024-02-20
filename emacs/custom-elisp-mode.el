9d112fcc7dc5457c7ad4b030d6a6b4119aab136e
Max Darling <maxdarling11@gmail.com>
2024-02-20 13:47:49 -0500

    emacs

---
+++ emacs/custom-elisp-mode.el
@@ -0,0 +1,18 @@
+;; goal: get this: http://xahlee.info/emacs/emacs/emacs_theme_gallery.html
+;; todo: more keyword highlighting (blue, gold, etc)
+;; this is from xah elisp mode ($32) http://xahlee.info/emacs/emacs/xah-elisp-mode.html
+;; I should just try to reverse-eng the colorscheme. a fun little starter project.
+
+;; (setq myvar)
+
+;; (defvar myvar 2 "ceinahitce")
+;; (defun mark-sexp (&optional arg allow-extend)
+;;   (interactive "ur mom")
+;;   (let ((hello 5))
+;;     (eq hello 1)))
+
+;; so, the var color is actually same as xah. but only a defvar is considered a
+;; variable, unlike the setqs or lets.
+;; note: C-u C-x = will show info about the char under cursor, including what
+;; font face it belongs to. the black words are not part of a font-face, that's
+;; the issue. So Xah has defined his own mappings for font-faces.
--- emacs/init.el
+++ emacs/init.el
@@ -1,17 +1,32 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Evil Setup
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Packages
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; evil
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-(package-refresh-contents)
+;; (add-to-list 'package-archives
+;;              '("melpa" . "https://melpa.org/packages/"))
+;; (package-initialize)
+;; (package-refresh-contents)
 
 (unless (package-installed-p 'evil)
   (package-install 'evil))
 
 (require 'evil)
 (evil-mode 1)
+
+;; ;; easymotion
+;; (unless (package-installed-p 'evil-easymotion)
+;;   (package-install 'evil-easymotion))
+
+;; avy
+(unless (package-installed-p 'avy)
+  (package-install 'avy))
+(setq avy-keys '(?a ?h ?e ?t ?i ?s ?c ?n ?y ?x ?w ?v ?u ?r ?p ?o ?m ?l ?k ?j ?g ?f ?d ?b))
+;; Xah valid critique of Avy: http://xahlee.info/emacs/misc/ace_jump_avy_vs_isearch.html 
+;; the only downside of search is that it alters jumplist. I agree that the brain cycle
+;; is pretty bad (or, good if you want typing game practice, lol). so, instead I should
+;; setup a screen-local search that doesn't pollute jump list.
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Binds
@@ -23,13 +38,47 @@
 
 (define-key evil-normal-state-map "gq" 'evil-indent)
 
+(define-key evil-normal-state-map "s" 'avy-goto-char)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Misc
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq make-backup-files nil) ; stop creating ~ files
+(setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
 
-(load-theme 'leuven-dark)
+(setq vc-follow-symlinks t)
+(setq use-short-answers t) 
+(setq visible-bell 1)
+(set-frame-font "Menlo-14" t t)
+(setq font-lock-maximum-decoration t)
+
+;; Xah theme
+;; todo: copy his syntax highlighting for elisp. see `custom-elisp-mode.el`
+(if (display-graphic-p)
+    (setq initial-frame-alist
+          '(
+            (tool-bar-lines . 0)
+            (width . 106)
+            (height . 60)
+            (background-color . "honeydew")
+            (left . 50)
+            (top . 50)))
+  (load-theme 'leuven-dark))
+
+(global-hl-line-mode 1)
 
 ;; fix evil undo and redo
 (global-undo-tree-mode)
 (evil-set-undo-system 'undo-tree)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(evil-easymotion undo-tree spacemacs-theme evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
