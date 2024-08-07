fe4d680eb10e959b03e8370ca6f2170848ab3b3c
Max Darling <maxdarling11@gmail.com>
2024-08-07 11:38:01 -0400

    Emacs: hide minor modes in modeline

--- emacs/lisp/theme.el
+++ emacs/lisp/theme.el
@@ -48,3 +48,26 @@
 (set-frame-font "Menlo-15" t t)
 (global-hl-line-mode 1)
 (blink-cursor-mode -1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Modeline
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; hide minor modes (https://emacs.stackexchange.com/a/3928)
+(defvar my//hidden-minor-modes
+  '(abbrev-mode
+    yas-minor-mode
+    auto-revert-mode
+    eldoc-mode
+    ;; auto-fill-function
+    ;; flycheck-mode
+    ;; flyspell-mode
+    ;; smooth-scroll-mode
+    ))
+(defun purge-minor-modes ()
+  (interactive)
+  (dolist (x my//hidden-minor-modes nil)
+    (let ((trg (cdr (assoc x minor-mode-alist))))
+      (when trg
+        (setcar trg "")))))
+(add-hook 'after-change-major-mode-hook 'purge-minor-modes)
