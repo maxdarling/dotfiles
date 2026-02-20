cf0ecf88f0b1a893916012d9b44fb2273a9414cd
Max Darling <maxdarling11@gmail.com>
2026-02-20 17:07:01 -0500

    emacs: implement my/comment-and-duplicate

--- emacs/lisp/mystuff.el
+++ emacs/lisp/mystuff.el
@@ -54,13 +54,53 @@ Based on `xah-run-current-file'"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Misc 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun my/comment-and-duplicate ()
+  "Copy the current line/region, comment it, and place it above point."
+  (interactive)
+  ;; region annoyance:
+  ;; - behavior: when run on region and cursor is on bottom line, an extra line is jumped.
+  ;; works fine for single-line and when cursor is on top line of region.
+  ;; - reason: internal impl of region, somewhere, represents the end of the region
+  ;; (recall: emacs region = between "mark" and "point") as beginning of line ("bol") of
+  ;; next line. in debug print below, point line number is 1 greater than we expect,
+  ;; i.e. on the next line.
+  ;; - solution: set a mark to jump back to (e.g. to start of region)
+  ;; - desired solution: have your cursor position appear unchanged before/after running command,
+  ;; i.e. a commented duplication appeared above you without movement. chatGPT 5.2 couldn't figure
+  ;; it out though - no prob, i'm quite happy with the current solution.
+
+  ;; (message "pt line=%d bolp=%S | mark line=%d | rb=%d re=%d"
+  ;;          (line-number-at-pos (point)) (bolp)
+  ;;          (line-number-at-pos (mark))
+  ;;          (line-number-at-pos (region-beginning))
+  ;;          (line-number-at-pos (region-end)))
+  (let* ((n-lines (if (use-region-p)
+		      (count-lines (region-beginning) (region-end))
+		    1))
+         (anchor (if (use-region-p) (region-beginning) (line-beginning-position))))
+    (duplicate-dwim 1) ;; region stays active!
+    (my/comment-line-dwim t) ;; don't advance point
+    ;; below: must deactivate manually for predictable movement.
+    ;; see command loop notes in 'deactivate-mark' for explainer.
+    (deactivate-mark) 
+    (goto-char anchor)
+    (forward-line n-lines)))
+
+(defun my/comment-line-dwim (&optional dont-advance-point)
+  "Comment line(s). Needed because `comment-line' on a region erroneously comments an extra line.
+  If `dont-advance-point' is t, don't move point to next line after commenting
+  (for single-line comments only)" 
+  (interactive)
+  (if (region-active-p)
+      (comment-dwim nil)
+    (comment-line 1)
+    (if dont-advance-point (previous-line))))
 
 ;; todo: fix for decimals, e.g 3.28 goes crazy...? must be a precision thing?
 ;; todo: fix removal of period, e.g. in "5."
 ;; todo: make number scoped only to right of cursor, e.g. 10.14.11 -> 10.14.12 intead of 10.15.11
 (defun my/inc-number-after-point (&optional incval)
   "Increment the current/next number on the current line by 1 (or `incval' if specified)."
-  (interactive)
   (let ((inc (or incval 1))
 	(origPoint (point)))
     (skip-chars-backward "0-9")
@@ -69,24 +109,13 @@ Based on `xah-run-current-file'"
 	(replace-match (number-to-string (+ inc (number-at-point))))
       (goto-char origPoint))))
 
-;; improve 'comment-line', and don't rely on evil-commentary (for fun + reduce bloat)
-;; issues:
-;; - visual line always comments out +1 lines
-;; - always moves your cursor to next line... (line or region) (can be good, but try off)
-;; note: next-line is good because you can dot it (?).
-(defun my/comment-dwim ()
-  (interactive)
-  (if (region-active-p)
-      (comment-dwim nil)
-    (comment-line 1)))
-
 ;; source: https://www.reddit.com/r/emacs/comments/13y5k9j/simple_fuzzy_find_file/
 (defun my/find-file-rec ()
   "Find a file recursively from the current working directory. rg highly recommended."
   (interactive)
   (let ((find-files-program
 	 (cond ((executable-find "rg") '("rg" "--color=never" "--files"))
-               ((executable-find "find") '("find" "." "-type" "f")))))
+	       ((executable-find "find") '("find" "." "-type" "f")))))
     (find-file
      (completing-read "Find file: " (apply #'process-lines find-files-program)))))
 
@@ -95,7 +124,7 @@ Based on `xah-run-current-file'"
   (interactive)
   (let ((find-files-program
 	 (cond ((executable-find "fd") '("fd" "--type=d"))
-               ((executable-find "find") '("find" "." "-type" "f")))))
+	       ((executable-find "find") '("find" "." "-type" "f")))))
     (find-file
      (completing-read "Find dir: " (apply #'process-lines find-files-program)))))
 
