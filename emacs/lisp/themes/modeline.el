1cd31d4ec5518b056421a8ecdc2b5bad58778c13
Max Darling <maxdarling11@gmail.com>
2026-02-28 13:26:17 -0500

    modeline crichton draft

--- emacs/lisp/themes/modeline.el
+++ emacs/lisp/themes/modeline.el
@@ -1,23 +1,79 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;  -*- lexical-binding: t; -*-
-;; Modeline
+;; -*- lexical-binding: t; -*-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; general 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; hide minor modes
+(setq mode-line-modes
+      '("[" (:propertize mode-name face mode-line-buffer-id) "]"))
 
-;; hide minor modes (https://emacs.stackexchange.com/a/3928)
-(defvar my//hidden-minor-modes
-  '(abbrev-mode
-    yas-minor-mode
-    auto-revert-mode
-    eldoc-mode
-    ;; auto-fill-function
-    ;; flycheck-mode
-    ;; flyspell-mode
-    ;; smooth-scroll-mode
-    ))
-(defun purge-minor-modes ()
-  (interactive)
-  (dolist (x my//hidden-minor-modes nil)
-    (let ((trg (cdr (assoc x minor-mode-alist))))
-      (when trg
-        (setcar trg "")))))
-(add-hook 'after-change-major-mode-hook 'purge-minor-modes)
+;; strip lexical binding hint (not working)
+(setq-default mode-name
+	          '(:eval (replace-regexp-in-string "/l\\'" "" (format-mode-line mode-name))))
 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; chrichton + karthinks v1
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; inspo:
+;; - https://www.youtube.com/watch?v=bnnacleqg6k
+;; - https://karthinks.com/software/it-bears-repeating/
+
+;; default
+;; (setq mode-line-format
+;;       ("%e" mode-line-front-space
+;;        (:propertize
+;;         ("" mode-line-mule-info mode-line-client mode-line-modified mode-line-remote
+;;          mode-line-window-dedicated)
+;;         display (min-width (6.0)))
+;;        mode-line-frame-identification mode-line-buffer-identification "   " mode-line-position
+;;        evil-mode-line-tag (project-mode-line project-mode-line-format) (vc-mode vc-mode) "  "
+;;        mode-line-modes mode-line-misc-info mode-line-end-spaces)
+;;       )
+
+;; todo
+;; - COLORS! (see prot examples?)
+;; - trim the lexical mode on elisp?
+
+;; backlog:
+;; - crichton: pads line and col separately? https://youtu.be/bnnacleqg6k?si=la7G6oNCJLLA99Pz&t=469
+
+(defvar-local my/mode-line--line-col
+  '((:eval (format "%-6s" (format-mode-line "%l:%C")))))
+
+(defvar-local my/mode-line--modified
+  '(mode-line-modified))
+  ;; '((:eval (if (buffer-modified-p) "*" "-"))))
+
+(defvar-local my/mode-line--evil-state
+  ;; evil-mode-line-tag " " ; bug: disappearing after entering insert mode
+  '((:eval (format-mode-line evil-mode-line-tag))))
+
+(defvar-local my/mode-line--proj-and-file
+  '((:eval
+     (let ((p (string-trim
+               (format-mode-line '(project-mode-line project-mode-line-format)))))
+       (if (string-empty-p p) "" (concat "[" p "]"))))
+    mode-line-buffer-identification))
+
+(defvar-local my/mode-line--vc-branch
+        '((:eval (when vc-mode
+                 (concat ":" (substring-no-properties vc-mode 5)))))) ; strip leading " Git:"
+
+(setq-default mode-line-format
+      `(
+	"%e"
+	,@my/mode-line--line-col
+	,@my/mode-line--modified 
+	,@my/mode-line--evil-state
+	,@my/mode-line--proj-and-file
+        "    "
+	" %p  "
+	,@my/mode-line--vc-branch "  "
+	mode-line-modes
+	mode-line-misc-info
+        mode-line-end-spaces))
+
+
+(kill-local-variable 'mode-line-format)
+(force-mode-line-update t)
