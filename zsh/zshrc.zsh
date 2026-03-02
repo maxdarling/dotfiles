759b448344d95e936868ef3088310713ef9e1d29
Max Darling <maxdarling11@gmail.com>
2026-03-02 16:08:32 -0500

    pyenv

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -126,3 +126,8 @@ eval "$(direnv hook zsh)"
 # bun
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
+
+# pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
