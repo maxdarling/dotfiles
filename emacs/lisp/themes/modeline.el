5609892aee89bf69f8cd1c59ddcb540bf2bd6204
Max Darling <maxdarling11@gmail.com>
2026-03-26 08:19:54 -0700

    emacs: modeline percent indicator fix

--- emacs/lisp/themes/modeline.el
+++ emacs/lisp/themes/modeline.el
@@ -1,45 +1,45 @@
-;; -*- lexical-binding: t; -*-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; general 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'subr-x)   ;; string-trim, string-empty-p
-(require 'project)  ;; project-current
+;;;; -*- lexical-binding: t; -*-
 
-;; hide minor modes
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; General
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'project)
+(require 'subr-x)
+
+;; Hide minor modes and keep just the major mode name.
 (setq mode-line-modes
       '("[" (:propertize mode-name face mode-line-buffer-id) "]"))
 
-;; strip lexical binding hint (not working)
-(setq-default mode-name
-	      '(:eval (replace-regexp-in-string "/l\\'" "" (format-mode-line mode-name))))
+;; Leaving this disabled for now: rewriting `mode-name` in terms of
+;; `(format-mode-line mode-name)` is self-referential.
+;; (setq-default mode-name
+;;               '(:eval (replace-regexp-in-string "/l\\'" "" (format-mode-line mode-name))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; notes (chrichton + karthinks v1)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; inspo:
-;; - https://www.youtube.com/watch?v=bnnacleqg6k
-;; - https://karthinks.com/software/it-bears-repeating/
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Notes (chrichton + karthinks v1)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; inspo:
+;;;; - https://www.youtube.com/watch?v=bnnacleqg6k
+;;;; - https://karthinks.com/software/it-bears-repeating/
+;;;;
+;;;; todo
+;;;; - COLORS! (see prot examples?)
+;;;; - add path prefix to the filename in projects
+;;;; - trim the lexical mode on elisp?
+;;;;
+;;;; backlog:
+;;;; - crichton: pads line and col separately? https://youtu.be/bnnacleqg6k?si=la7G6oNCJLLA99Pz&t=469
 
-;; todo
-;; - COLORS! (see prot examples?)
-;; - add path prefix to the filename in projects
-;; - trim the lexical mode on elisp?
-
-;; backlog:
-;; - crichton: pads line and col separately? https://youtu.be/bnnacleqg6k?si=la7G6oNCJLLA99Pz&t=469
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Faces
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Faces
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defface my-modeline-vc-face
   '((t :inherit mode-line
-       :foreground "brown"
-       ))
+       :foreground "brown"))
   "Face for VC branch segment.")
 
 (defface my-modeline-project-face
-  '((t :inherit mode-line
-       ))
+  '((t :inherit mode-line))
   "Face for project segment.")
 
 (defface my-modeline-buffer-face
@@ -52,18 +52,20 @@
        :weight bold))
   "Face for Evil state segment.")
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Helpers
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Helpers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun my/ml--with-face (face s)
   (when (and s (not (string-empty-p s)))
     (propertize s 'face face)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Segments
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun my/ml-modified ()
-  (format-mode-line mode-line-modified))
+(defun my/ml--truncate (s width)
+  (when s
+    (truncate-string-to-width s width nil nil t)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Segments
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun my/ml-evil-state ()
   (when (boundp 'evil-mode-line-tag)
@@ -72,26 +74,25 @@
      (format-mode-line (or evil-mode-line-tag " ")))))
 
 (defun my/ml-project ()
-  (let ((proj (project-current nil))) ; nil = don’t prompt
+  (let ((proj (project-current nil)))
     (when proj
       (let* ((root (expand-file-name (project-root proj)))
              (name (file-name-nondirectory
                     (directory-file-name root))))
         (my/ml--with-face
          'my-modeline-project-face
-         (format "[%s]" name))))))
+         (format "[%s]" (my/ml--truncate name 18)))))))
 
 (defun my/ml-buffer-name ()
   (my/ml--with-face
    'my-modeline-buffer-face
-   (format-mode-line mode-line-buffer-identification)))
+   (my/ml--truncate
+    (format-mode-line mode-line-buffer-identification)
+    40)))
 
 (defun my/ml-line-col ()
-  ;; (format " %-7s" (format-mode-line "%l:%c")))
+  ;; (format " %-7s" (format-mode-line "%l:%c"))
   (format " %-3s" (format-mode-line "%l")))
-
-(defun my/ml-percent ()
-  (format-mode-line " %p  "))
 
 (defun my/ml-vc-branch ()
   (when vc-mode
@@ -99,20 +100,20 @@
      'my-modeline-vc-face
      (concat ":" (substring-no-properties vc-mode 5)))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Assembly
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Assembly
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq-default
  mode-line-format
  '("%e"
-   (:eval (my/ml-modified))
+   mode-line-modified
    " "
    (:eval (my/ml-evil-state))
    (:eval (my/ml-project))
    (:eval (my/ml-buffer-name))
    "    "
    (:eval (my/ml-line-col))
-   (:eval (my/ml-percent))
+   " %p  "
    (:eval (my/ml-vc-branch))
    "  "
    mode-line-modes
